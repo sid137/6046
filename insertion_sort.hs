f5f228cda69f297796b23931c9119ab8047264e2
Sidney Burks <sid137@gmail.com>
2012-02-12 05:08:07 +0100

    addding reverse_insertion_sort

--- insertion_sort.hs
+++ insertion_sort.hs
@@ -4,11 +4,23 @@ list = [3,0,9,4,2,8,6, 7]
 
 insert :: (Ord a) => a -> [a] -> [a]
 insert x [] = [x]
-insert x (y:ys)
+insert x list@(y:ys)
   | x > y = y:(insert x ys)
-  | x <= y = x:y:ys 
+  | otherwise = x:list
 
 insertion_sort :: (Ord a) => [a] -> [a]
 insertion_sort [x] = [x]
 insertion_sort (x:xs) = 
   insert x (insertion_sort xs)
+
+reverse_insert :: (Ord a) => a -> [a] -> [a]
+reverse_insert x [] = [x]
+reverse_insert x (y:ys) 
+  | x > y = x:y:ys
+  | otherwise = y:( reverse_insert x ys)
+
+reverse_insertion_sort :: (Ord a) => [a] -> [a]
+reverse_insertion_sort [x] = [x]
+reverse_insertion_sort (x:xs) =
+  reverse_insert x (reverse_insertion_sort xs)
+
